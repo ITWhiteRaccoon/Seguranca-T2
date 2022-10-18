f4973511143c7a635ba0a6965a1565b1604cc437
Edu Andrade <eduardo.a@edu.pucrs.br>
2022-10-18 08:25:44 -0300

    Started looping

--- Program.cs
+++ Program.cs
@@ -5,25 +5,40 @@ namespace T2_SHA256;
 
 public class Program
 {
-    private static readonly int ChunkSize = 1024;
+    private const int ChunkSize = 1024;
 
     public static void Main(string[] args)
     {
-        var fileName = "C:/Dev/T2-SHA256/Dados/FuncoesResumo - SHA1.mp4";
-        using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-        {
-            var lastChunkSize = fs.Length % ChunkSize;
-            using (BinaryReader br = new BinaryReader(fs, Encoding.ASCII))
-            {
-                br.BaseStream.Position = fs.Length - lastChunkSize;
-                byte[] chunk = br.ReadBytes(ChunkSize);
+        const string fileName = "C:/Dev/T2-SHA256/Dados/FuncoesResumo - SHA1.mp4";
 
-                using (var sha256 = SHA256.Create())
-                {
-                    var hash = sha256.ComputeHash(chunk);
-                    Console.WriteLine(BitConverter.ToString(hash).Replace("-", "").ToLower());
-                }
-            }
+        CalculateHash(fileName);
+    }
+
+    private static byte[] CalculateHash(string fileName)
+    {
+        using var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs, Encoding.ASCII);
+        using var sha256 = SHA256.Create();
+
+        long lastChunkSize = fs.Length % ChunkSize;
+        var chunkCount = (int)((fs.Length - lastChunkSize) / ChunkSize);
+
+        br.BaseStream.Position = fs.Length - lastChunkSize;
+        byte[] chunk = br.ReadBytes(ChunkSize);
+
+        byte[] hash = sha256.ComputeHash(chunk);
+        Console.WriteLine(Convert.ToHexString(hash));
+
+        for (int i = chunkCount - 1; i >= 0; i++)
+        {
+            br.BaseStream.Position = i * ChunkSize;
+            chunk = br.ReadBytes(ChunkSize);
+            var block = new byte[chunk.Length + hash.Length];
+            Buffer.BlockCopy(chunk, 0, block, 0, chunk.Length);
+            Buffer.BlockCopy(hash, 0, block, chunk.Length, hash.Length);
+            hash = sha256.ComputeHash(block);
         }
+
+        return null;
     }
 }
